d36f6c304bcfed9c0934641633fe0fc71f30e56d
KGL2011 <152817667+KGL2011@users.noreply.github.com>
2023-12-03 15:05:53 -0800

    Update streamlit_app.py
    fixed spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlet.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
